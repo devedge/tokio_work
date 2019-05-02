c82a63333ba86edd2e8c77825dc3d957c93aa38e
devedge <richard.pjski2@gmail.com>
2019-05-01 21:42:08 -0400

    Echo server using tokio::spawn

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 extern crate tokio;
 
 use tokio::io;
-use tokio::net::TcpStream;
+use tokio::net::TcpListener;
 use tokio::prelude::*;
+
+// after starting, run in another terminal:
+// ncat localhost 6142
 
 fn main() {
     let addr = "127.0.0.1:6142".parse().unwrap();
-    let client = TcpStream::connect(&addr)
-        .and_then(|stream| {
-            println!("created stream");
+    let listener = TcpListener::bind(&addr).unwrap();
 
-            io::write_all(stream, "hello world\n").then(|result| {
-                println!("wrote to stream; success={:?}", result.is_ok());
+    // convert TcpListener to a stream of incoming connections
+    let server = listener
+        .incoming()
+        .for_each(|socket| {
+            let (reader, writer) = socket.split();
+            let amount = io::copy(reader, writer);
+
+            let msg = amount.then(|result| {
+                match result {
+                    Ok((amount, _, _)) => println!("wrote {} bytes", amount),
+                    Err(e) => println!("error: {}", e),
+                }
+
                 Ok(())
-            })
+            });
+
+            tokio::spawn(msg);
+            Ok(())
         })
         .map_err(|err| {
-            println!("connect error = {:?}", err);
+            println!("accept error = {:?}", err);
         });
 
-    println!("About to create the stream and write to it...");
-    tokio::run(client);
-    println!("Stream has been created and written to.");
+    println!("server running on localhost:6142");
+
+    // start the server
+    tokio::run(server);
 }
