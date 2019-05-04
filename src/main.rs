dd3ce1a6376f6ad28e61125b4b1dc5aa09bc2d60
devedge <richard.pjski2@gmail.com>
2019-05-04 10:08:58 -0400

    Chain computations and write to a TCP socket

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
+extern crate bytes;
 #[macro_use]
 extern crate futures;
 extern crate tokio;
 
+use bytes::{Buf, Bytes};
 use futures::{Async, Future, Poll};
+use std::io::{self, Cursor};
+use tokio::io::AsyncWrite;
 use tokio::net::{tcp::ConnectFuture, TcpStream};
 
-struct GetPeerAddr {
-    connect: ConnectFuture,
+enum HelloWorld {
+    Connecting(ConnectFuture),
+    Connected(TcpStream, Cursor<Bytes>),
 }
 
-impl Future for GetPeerAddr {
+impl Future for HelloWorld {
     type Item = ();
-    type Error = ();
+    type Error = io::Error;
 
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        match self.connect.poll() {
-            Ok(Async::Ready(socket)) => {
-                println!("peer address = {}", socket.peer_addr().unwrap());
-                Ok(Async::Ready(()))
-            }
-            Ok(Async::NotReady) => Ok(Async::NotReady),
-            Err(e) => {
-                println!("failed to connect: {}", e);
-                Ok(Async::Ready(()))
+    fn poll(&mut self) -> Poll<(), io::Error> {
+        use self::HelloWorld::*;
+
+        loop {
+            match self {
+                Connecting(ref mut f) => {
+                    let socket = try_ready!(f.poll());
+                    let data = Cursor::new(Bytes::from_static(b"hello world"));
+                    *self = Connected(socket, data);
+                }
+                Connected(ref mut socket, ref mut data) => {
+                    // Keep trying to write the buffer to the socket as long as the
+                    // buffer has more bytes available for consumption
+                    while data.has_remaining() {
+                        try_ready!(socket.write_buf(data));
+                    }
+                    return Ok(Async::Ready(()));
+                }
             }
         }
     }
 }
 
 fn main() {
-    let addr = "192.168.0.152:1234".parse().unwrap();
+    let addr = "127.0.0.1:1234".parse().unwrap();
     let connect_future = TcpStream::connect(&addr);
-    let get_peer_addr = GetPeerAddr {
-        connect: connect_future,
-    };
+    let hello_world = HelloWorld::Connecting(connect_future);
 
-    tokio::run(get_peer_addr);
+    // Run it, here we map the error since tokio::run expects a Future<Item=(), Error=()>
+    tokio::run(hello_world.map_err(|e| println!("{0}", e)))
 }
