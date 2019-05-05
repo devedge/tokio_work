c76aacdb343f6479141157b08803f18db5969637
devedge <richard.pjski2@gmail.com>
2019-05-04 23:06:18 -0400

    Synchronous stream implementation of Fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,76 @@
+#[macro_use]
 extern crate futures;
 extern crate tokio;
 
-use futures::Future;
-use tokio::io;
-use tokio::net::TcpStream;
+use futures::{Async, Future, Poll, Stream};
+use std::fmt;
+
+pub struct Fibonacci {
+    curr: u64,
+    next: u64,
+}
+
+impl Fibonacci {
+    fn new() -> Fibonacci {
+        Fibonacci { curr: 1, next: 1 }
+    }
+}
+
+impl Stream for Fibonacci {
+    type Item = u64;
+
+    // The stream will never yield an error
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Option<u64>, ()> {
+        let curr = self.curr;
+        let next = curr + self.next;
+
+        self.curr = self.next;
+        self.next = next;
+
+        Ok(Async::Ready(Some(curr)))
+    }
+}
+
+pub struct Display10<T> {
+    stream: T,
+    curr: usize,
+}
+
+impl<T> Display10<T> {
+    fn new(stream: T) -> Display10<T> {
+        Display10 { stream, curr: 0 }
+    }
+}
+
+impl<T> Future for Display10<T>
+where
+    T: Stream,
+    T::Item: fmt::Display,
+{
+    type Item = ();
+    type Error = T::Error;
+
+    fn poll(&mut self) -> Poll<(), Self::Error> {
+        while self.curr < 10 {
+            let value = match try_ready!(self.stream.poll()) {
+                Some(value) => value,
+                // there were less than 10 values to display, terminate the future
+                None => break,
+            };
+
+            println!("value #{} = {}", self.curr, value);
+            self.curr += 1;
+        }
+
+        Ok(Async::Ready(()))
+    }
+}
 
 fn main() {
-    // test by running this in anothe terminal:
-    // ncat -l 127.0.0.1 1234
-    let addr = "127.0.0.1:1234".parse().unwrap();
+    let fib = Fibonacci::new();
+    let display = Display10::new(fib);
 
-    let future = TcpStream::connect(&addr)
-        .and_then(|socket| io::write_all(socket, b"hello world"))
-        .map(|_| println!("write complete"))
-        .map_err(|_| println!("failed"));
-
-    tokio::run(future);
+    tokio::run(display);
 }
