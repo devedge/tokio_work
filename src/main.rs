871d3d0e6765dffc6419a838e7b09e28b73906ac
devedge <richard.pjski2@gmail.com>
2019-05-07 00:31:23 -0400

    Spawn tasks with lazy

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,17 @@
 extern crate futures;
 extern crate tokio;
 
-use futures::{stream, Stream};
-use std::time::Duration;
-use tokio::timer::Interval;
-
-struct Fibonacci {
-    curr: u64,
-    next: u64,
-}
+use futures::future::lazy;
 
 fn main() {
-    let mut fib = Fibonacci { curr: 1, next: 1 };
+    tokio::run(lazy(|| {
+        for i in 0..4 {
+            tokio::spawn(lazy(move || {
+                println!("Hello from task {}", i);
+                Ok(())
+            }));
+        }
 
-    let future = Interval::new_interval(Duration::from_secs(1)).map(move |_| {
-        let curr = fib.curr;
-        let next = curr + fib.next;
-
-        fib.curr = fib.next;
-        fib.next = next;
-
-        curr
-    });
-
-    tokio::run(future.take(10).map_err(|_| ()).for_each(|num| {
-        println!("{}", num);
         Ok(())
     }));
-
-    // use concrete streams to convert values and iterators info streams
-    let values = vec!["one", "two", "three"];
-
-    tokio::run(stream::iter_ok(values).for_each(|value| {
-        println!("{}", value);
-        Ok(())
-    }))
 }
