a99cc2f066571ee75d3fe0df30c0a3ded68dd7a7
devedge <richard.pjski2@gmail.com>
2019-05-07 01:21:13 -0400

    MPSC channel with lazy spawn

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@ extern crate futures;
 extern crate tokio;
 
 use futures::future::lazy;
-use futures::sync::oneshot;
-use futures::Future;
+use futures::sync::mpsc;
+use futures::{stream, Future, Sink, Stream};
 
 fn main() {
     tokio::run(lazy(|| {
-        let (tx, rx) = oneshot::channel();
+        let (tx, rx) = mpsc::channel(1_024);
 
         tokio::spawn(lazy(|| {
-            tx.send("hello from spawned task");
-            Ok(())
+            stream::iter_ok(0..10).fold(tx, |tx, i| {
+                tx.send(format!("Message {} from spawned task", i))
+                    .map_err(|e| println!("error = {:?}", e))
+            })
+            .map(|_| ()) // drop the tx handle
         }));
 
-        rx.and_then(|msg| {
+        rx.for_each(|msg| {
             println!("Got `{}`", msg);
             Ok(())
         })
-        .map_err(|e| println!("error = {:?}", e))
     }));
 }
